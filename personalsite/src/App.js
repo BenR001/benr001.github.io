5710ec24cbbf6d57e9c9eb1a09eb6a6cad5c8b5b
brocean13 <benjir11@yahoo.com>
2023-05-26 22:40:13 -0400

    test commit

---
+++ personalsite/src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+import Body from './components/Body'
+
+
+
+
+
+function Footer() {
+  return <div>Designed by Ben, THIS IS THE FOOTER</div>
+}
+
+function App() {
+  return (
+        <div className="App">
+          <h1>HEY BEN!</h1>
+        <Body/>
+        <Footer/>
+        </div>
+  );
+}
+
+export default App;
